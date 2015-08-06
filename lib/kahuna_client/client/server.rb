3016f68969503865b72e5f890ae7a1e8a4ec4e0a
Parov <pierangelo.turchiarelli@gmail.com>
2015-08-06 16:56:53 +0200

    [ENHANCEMENT] added support for user attributes

--- lib/kahuna_client/client/server.rb
+++ lib/kahuna_client/client/server.rb
@@ -3,10 +3,15 @@ module KahunaClient
     module Server
 
       # Sends user related events to Kahuna via Server API
-      #
-      #
-      #
-      def send_event(key:key, dev_id:dev_id, env:env, username:username, user_email:user_email, event:event)
+      # key (e.g. key = dc973328798042a6b5d02c41c57508a4) Your Account's Secret Key
+      # dev_id (e.g. dev_id = 12a217b6ui2) A unique id for the device.
+      # env (e.g. env = p) Environment identifier. This identifies if you are hitting your sandbox account or production
+      #                    account. The 2 possible value are "s" and "p".
+      # username (e.g. username = johnd1989) The globally unique username or user id of the user.
+      # user_email (e.g. email = jdoe@usekahuna.com) Email Address of the User
+      # event (e.g. event = start) Event Name
+      # user_info (e.g. user_info = {'first_name': 'John', 'last_name': 'Doe', 'gender': 'm'})
+    def send_event(key:key, dev_id:dev_id, env:env, username:username, user_email:user_email, event:event, user_info:user_info)
         params = {
             :key => key,
             :dev_id => dev_id,
@@ -14,6 +19,7 @@ module KahunaClient
             :username => username,
             :user_email => user_email,
             :event => event,
+            :user_info => user_info,
             :only_params => true
         }
 
--- spec/kahuna_client/client/server_spec.rb
+++ spec/kahuna_client/client/server_spec.rb
@@ -1,21 +1,23 @@
 require "spec_helper"
+require "json"
 
 describe KahunaClient::Client do
   describe "#send_event" do
 
     subject {
       client.send_event key:  app_key,
-                              dev_id: dev_id,
-                              env: environment,
-                              username: username,
-                              user_email: user_email,
-                              event: event
+                        dev_id: dev_id,
+                        env: environment,
+                        username: username,
+                        user_email: user_email,
+                        event: event,
+                        user_info: user_info
     }
 
     let(:client) {
       KahunaClient::Client.new app_id:  app_id,
-                                        app_key: app_key,
-                                        environment: environment
+                               app_key: app_key,
+                               environment: environment
     }
 
     let(:app_id) { "id" }
@@ -25,6 +27,13 @@ describe KahunaClient::Client do
     let(:username) { "test@email.com" }
     let(:user_email) { "test@email.com" }
     let(:event) { "signup" }
+    let(:user_info) {
+      {
+          "first_name" => "John",
+          "last_name" => "Doe",
+          "gender" => "m"
+      }
+    }
 
     let(:params) {
       {
@@ -34,18 +43,19 @@ describe KahunaClient::Client do
           username: username,
           user_email: user_email,
           event: event,
+          user_info: user_info,
           only_params: true
       }
     }
 
     before do
-      stub_post("log?dev_id=12345678&env=s&event=signup&key=key&user_email=test@email.com&username=test@email.com")
+      stub_post("log?dev_id=12345678&env=s&event=signup&key=key&user_email=test@email.com&user_info%5Bfirst_name%5D=John&user_info%5Bgender%5D=m&user_info%5Blast_name%5D=Doe&username=test@email.com")
           .to_return(:body => fixture('success.json'))
     end
 
     it "get the correct resource" do
       subject
-      expect(a_post("log?dev_id=12345678&env=s&event=signup&key=key&user_email=test@email.com&username=test@email.com")).to have_been_made.once
+      expect(a_post("log?dev_id=12345678&env=s&event=signup&key=key&user_email=test@email.com&user_info%5Bfirst_name%5D=John&user_info%5Bgender%5D=m&user_info%5Blast_name%5D=Doe&username=test@email.com")).to have_been_made.once
     end
   end
 end
